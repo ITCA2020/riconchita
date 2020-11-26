3e654e953f0d751a617e03c880e522a23acbb12f
e-slzr <eliiuslzr@gmail.com>
2020-11-26 08:29:39 -0600

    nuevos activities

--- app/src/main/java/sv/edu/itca/restaurante_rinconchita/MenuPrincipal.java
+++ app/src/main/java/sv/edu/itca/restaurante_rinconchita/MenuPrincipal.java
@@ -5,76 +5,90 @@ import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.TextView;
 
-public class Home extends AppCompatActivity {
-    CardView btnAlmuerzos, btnDesayunos, btnAperitivos, btnPostres, btnBebidas, btnCarrito;
-    ImageView btnMenu;
-
+public class MenuPrincipal extends AppCompatActivity {
+    TextView btnHome, btnAlmuerzos, btnDesayunos, btnAperitivos, btnPostres, btnBebidas, btnCarrito, btnHistorial, btnCerrarSesion, btnSalir;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_menu_principal);
 
+        btnHome = findViewById(R.id.btnHome);
         btnAlmuerzos = findViewById(R.id.btnAlmuerzos);
         btnAperitivos = findViewById(R.id.btnAperitivos);
         btnBebidas = findViewById(R.id.btnBebidas);
         btnCarrito = findViewById(R.id.btnCarrito);
         btnDesayunos = findViewById(R.id.btnDesayunos);
         btnPostres = findViewById(R.id.btnPostres);
-        btnMenu = findViewById(R.id.btnMenu);
+        btnHistorial = findViewById(R.id.btnHistorial);
+        btnCerrarSesion = findViewById(R.id.btnCerraSesion);
+        btnSalir = findViewById(R.id.btnSalir);
 
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent objIntento = new Intent(MenuPrincipal.this, Home.class);
+                startActivity(objIntento);
+            }
+        });
         btnAlmuerzos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent objIntento = new Intent(Home.this, Almuerzos.class);
+                Intent objIntento = new Intent(MenuPrincipal.this, Almuerzos.class);
                 startActivity(objIntento);
             }
         });
         btnAperitivos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent objIntento = new Intent(Home.this, Aperitivos.class);
+                Intent objIntento = new Intent(MenuPrincipal.this, Aperitivos.class);
                 startActivity(objIntento);
             }
         });
         btnBebidas.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent objIntento = new Intent(Home.this, Bebidas.class);
+                Intent objIntento = new Intent(MenuPrincipal.this, Bebidas.class);
                 startActivity(objIntento);
             }
         });
         btnCarrito.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent objIntento = new Intent(Home.this, Carrito.class);
+                Intent objIntento = new Intent(MenuPrincipal.this, Carrito.class);
                 startActivity(objIntento);
             }
         });
         btnDesayunos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent objIntento = new Intent(Home.this, Desayunos.class);
+                Intent objIntento = new Intent(MenuPrincipal.this, Desayunos.class);
                 startActivity(objIntento);
             }
         });
         btnPostres.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent objIntento = new Intent(Home.this, Postres.class);
+                Intent objIntento = new Intent(MenuPrincipal.this, Postres.class);
                 startActivity(objIntento);
             }
         });
-        btnMenu.setOnClickListener(new View.OnClickListener() {
+        btnCerrarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent objIntento = new Intent(Home.this, MenuPrincipal.class);
+                Intent objIntento = new Intent(MenuPrincipal.this, Login.class);
                 startActivity(objIntento);
+                finish();
+            }
+        });
+        btnSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
             }
         });
     }
-
 }
